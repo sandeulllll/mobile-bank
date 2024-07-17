e3971312d37e9aff64dd6a7846cb20ac7b219ab6
py <py820901@163.com>
2024-07-17 19:53:32 +0800

    修改pom文件，设置xml文件夹路径。

---
+++ src/main/java/com/ccnu/mobilebank/support/UserSupport.java
@@ -0,0 +1,24 @@
+package com.ccnu.mobilebank.support;
+
+import com.ccnu.mobilebank.pojo.exception.ConditionException;
+import com.ccnu.mobilebank.util.TokenUtil;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class UserSupport {
+
+    public Integer getCurrentMobileId(){
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        String token = requestAttributes.getRequest().getHeader("token");
+        Integer mobileId = TokenUtil.verifyToken(token).get(0);
+        return mobileId;
+    }
+    public Integer getCurrentPersonId(){
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        String token = requestAttributes.getRequest().getHeader("token");
+        Integer personId = TokenUtil.verifyToken(token).get(1);
+        return personId;
+    }
+}
