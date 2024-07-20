528581f93ef33ec8483668a7e5642ada056c5989
py <py820901@163.com>
2024-07-20 14:03:28 +0800

    修改拦截器里获取token并校验的代码的部分

--- src/main/java/com/ccnu/mobilebank/handler/TokenInterceptor.java
+++ src/main/java/com/ccnu/mobilebank/handler/TokenInterceptor.java
@@ -5,6 +5,8 @@ import com.ccnu.mobilebank.util.TokenUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.util.List;
@@ -14,7 +16,9 @@ public class TokenInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String token = request.getHeader("Authorization");
+//        String token = request.getHeader("Authorization");
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        String token = requestAttributes.getRequest().getHeader("token");
 
         if (token == null || token.isEmpty()) {
             throw new ConditionException("597", "Token is missing");
